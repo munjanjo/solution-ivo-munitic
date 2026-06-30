f50d306d39ce9f6cee0a7aaff2c48d113cd9a8d6
munjanjo <ivo.munitic11@gmail.com>
2026-06-30 18:46:11 +0200

    Initial Spring Boot project setup

---
+++ src/main/java/org/example/abysaltobackendtask/AbysaltoBackendTaskApplication.java
@@ -0,0 +1,13 @@
+package org.example.abysaltobackendtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbysaltoBackendTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AbysaltoBackendTaskApplication.class, args);
+    }
+
+}
